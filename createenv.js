031c8d52aed92990d186ecb9b50e632e9887c572
esimed <contact@esimed.fr>
2023-03-23 14:42:00 +0100

    Platform.sh

--- createenv.js
+++
@@ -1,7 +0,0 @@
-var myArgs = process.argv.slice(2)
-var relationships = myArgs[0]
-var db = myArgs[1]
-var json_rel = JSON.parse(relationships)
-var json_db = json_rel[db][0]
-console.log("CONNECTION_STRING=postgres://"+json_db["username"]+":"+json_db["password"]+"@"+json_db["host"]+":"+json_db["port"]+"/"+json_db["path"])
-
